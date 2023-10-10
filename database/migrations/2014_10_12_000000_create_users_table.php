a774824790f2f5aa2bf5bf69f1a75751d8a82e93
DESKTOP-3UQB6L5\Majas <oskars.morics@ces.lv>
2023-10-10 20:10:16 +0300

    Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('surname');
-            $table->string('description');
-            $table->string('skills');//TODO Jāpardomā
+            $table->string('description')->nulable();;
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
